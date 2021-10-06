9586e07c543095385b567b46aff2889067a2b1ab
madan <madan9>
2021-10-06 10:44:07 +0545

    Implemented CRUD operations using firestore database

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/services/auth_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backwardsCompatibility: false,
-        title: Text('Home'),
-        centerTitle: true,
-        backgroundColor: Colors.pink,
-        actions: [
-          TextButton.icon(
-            onPressed: () async {
-              await AuthService().signOut();
-            },
-            icon: Icon(Icons.logout),
-            label: Text('Logout'),
-            style: TextButton.styleFrom(primary: Colors.white),
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('Welcome to the home screen'),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          backwardsCompatibility: false,
+          title: Text('Home'),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          actions: [
+            TextButton.icon(
+              onPressed: () async {
+                await AuthService().signOut();
+              },
+              icon: Icon(Icons.logout),
+              label: Text('Logout'),
+              style: TextButton.styleFrom(primary: Colors.white),
+            )
+          ],
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  CollectionReference users = firestore.collection('users');
+                  // await users.add({'name': 'Rahul Agrawal'});
+                  await users.doc('flutter123').set({'name': 'Google Flutter'});
+                },
+                child: Text('Add Data to Firestore'),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    CollectionReference users = firestore.collection('users');
+                    // QuerySnapshot allResults = await users.get();
+                    // allResults.docs.forEach((DocumentSnapshot result) {
+                    //   print(result.data());
+                    // });
+                    DocumentSnapshot result =
+                        await users.doc('flutter123').get();
+                    print(result.data());
+
+                    // users.doc('flutter123').snapshots().listen((result) {
+                    //   print(result.data());
+                    // });
+                  },
+                  child: Text("Read data from firestore")),
+              ElevatedButton(
+                  onPressed: () async {
+                    await firestore
+                        .collection('users')
+                        .doc('flutter123')
+                        .update({'name': 'Flutter Firebase'});
+                  },
+                  child: Text('update Data in firestore')),
+              ElevatedButton(
+                  onPressed: () async {
+                    await firestore
+                        .collection('users')
+                        .doc('flutter123')
+                        .delete();
+                  },
+                  child: Text('delete data from firestore'))
+            ],
+          ),
+        ));
   }
 }
