110ee3bb816a37bda1f746ae108606ce82a37bd4
madan <madan9>
2021-10-06 18:43:25 +0545

    Configured Firebase Storage and working on UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase/screens/home_screen.dart';
 // import 'package:flutter_firebase/screens/home_screen.dart';
 import 'package:flutter_firebase/screens/register_screen.dart';
+import 'package:flutter_firebase/screens/upload_image.dart';
 import 'package:flutter_firebase/services/auth_service.dart';
 
 void main() async {
@@ -23,7 +24,8 @@ class MyApp extends StatelessWidget {
             stream: AuthService().firebaseAuth.authStateChanges(),
             builder: (context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                return HomeScreen(snapshot.data);
+                // return HomeScreen(snapshot.data);
+                return UploadImageScreen();
               }
               return RegisterScreen();
             }));
