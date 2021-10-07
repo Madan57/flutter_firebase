91a5e0e8fef9e109cb5f6111d77a4cacb5eaf688
madan <madan9>
2021-10-07 08:16:21 +0545

    Implementing Image Upload

--- lib/screens/upload_image.dart
+++ lib/screens/upload_image.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadImageScreen extends StatefulWidget {
   const UploadImageScreen({Key? key}) : super(key: key);
@@ -8,6 +12,51 @@ class UploadImageScreen extends StatefulWidget {
 }
 
 class _UploadImageScreenState extends State<UploadImageScreen> {
+  FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+  bool loading = false;
+
+  Future<void> uploadImage(String inputSource) async {
+    final picker = ImagePicker();
+    final XFile? pickedImage = await picker.pickImage(
+        source:
+            inputSource == 'camera' ? ImageSource.camera : ImageSource.gallery);
+
+    if (pickedImage == null) {
+      return null;
+    }
+    String fileName = pickedImage.name;
+    File imageFile = File(pickedImage.path);
+
+    try {
+      setState(() {
+        loading = true;
+      });
+      await firebaseStorage.ref(fileName).putFile(imageFile);
+      setState(() {
+        loading = false;
+      });
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Successfully Uploaded')));
+    } on FirebaseException catch (e) {
+      print(e);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<List> loadImages() async {
+    List<Map> files = [];
+    final ListResult result = await firebaseStorage.ref().listAll();
+    final List<Reference> allFiles = result.items;
+
+    await Future.forEach(allFiles, (Reference file) async {
+      final String fileUrl = await file.getDownloadURL();
+      files.add({"url": fileUrl, "path": file.fullPath});
+    });
+    print(files);
+    return files;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +68,25 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.camera),
-                    label: Text('Camera')),
-                ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.library_add),
-                    label: Text('Gallery'))
-              ],
-            )
+            loading
+                ? Center(child: CircularProgressIndicator())
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton.icon(
+                          onPressed: () {
+                            uploadImage("camera");
+                          },
+                          icon: Icon(Icons.camera),
+                          label: Text('Camera')),
+                      ElevatedButton.icon(
+                          onPressed: () {
+                            uploadImage("gallery");
+                          },
+                          icon: Icon(Icons.library_add),
+                          label: Text('Gallery'))
+                    ],
+                  )
           ],
         ),
       ),
